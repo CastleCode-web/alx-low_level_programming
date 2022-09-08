90d69a7f9d44410f242c9393ffb13b29e163e47f
Akanni Feyisara <creativeslides@outlook.com>
2022-09-08 16:11:55 -0700

    Write a script program that prints an output followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/* Here is a script that utilizes printf function to print */
+
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	Return (0);
+
+}
