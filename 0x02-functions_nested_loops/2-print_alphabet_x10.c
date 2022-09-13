fef0d9221d793548e1999c14429c072949817a8a
Akanni Feyisara <creativeslides@outlook.com>
2022-09-13 10:39:45 -0700

    Write a function that prints 10x its alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * print_alphabet_x10 - prints alphabet of letters 10x
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
