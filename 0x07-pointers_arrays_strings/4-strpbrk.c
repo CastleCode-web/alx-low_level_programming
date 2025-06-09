9098df4cf0cdfd4c67e5e6fe6230eb3fe218bfcd
Castle <creativeslides@outlook.com>
2025-06-09 11:29:23 +0100

    Fix betty character number error

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,23 +1,27 @@
 #include "main.h"
+
 /**
- * _strpbrk -> function that searches a string for any of a set of bytes
- * @s: the string to be checked with xter in accept
- * @accept: the string byte to be checked with that of s
- * Return: returns a pointer to the bytes in s that matches the bytes in accept,
- * or NULL, if no such byte is found
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to be searched
+ * @accept: string to seache from
+ *
+ * Return: a pointer to the byte in s that matches that of
+ * the byte in accept, or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		i = 0;
+		while (accept[i] != '\0')
 		{
-			if (s[i] == accept[j])
-				return (&s[i]);
+			if (*s == accept[i])
+				return (s);
+			i++;
 		}
+		s++;
 	}
-
 	return (NULL);
 }
